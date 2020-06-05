8197552b8ebcc91263660a5a63afc756299ce701
d3ward <eduardursu78@gmail.com>
2020-06-06 00:21:18 +0200

    Update MainController.java

--- taskmanager_3/src/main/java/it/uniroma3/siw/taskmanager/controller/MainController.java
+++ taskmanager_3/src/main/java/it/uniroma3/siw/taskmanager/controller/MainController.java
@@ -1,15 +1,9 @@
 package it.uniroma3.siw.taskmanager.controller;
 
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import javax.servlet.http.HttpSession;
 
 /**
  * The MainController is a Spring Boot Controller to handle the generic
